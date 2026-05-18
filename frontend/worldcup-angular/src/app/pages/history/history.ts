303bb8e251b56c03b39bbf65dcb89325bae99f94
mousaghouri <2502130@students.au.edu.pk>
2026-05-19 00:49:19 +0500

    Day 2: History page completed with all 22 World Cup tournaments

--- frontend/worldcup-angular/src/app/pages/history/history.ts
+++ frontend/worldcup-angular/src/app/pages/history/history.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-history',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './history.html',
-  styleUrl: './history.css',
+  styleUrl: './history.css'
 })
 export class History {}
